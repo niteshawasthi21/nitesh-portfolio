419f4d1f3e83cb584cf2b39860662e1ee03d5bb2
niteshawasthi21 <tonyawasthi8586@gmail.com>
2026-05-10 00:14:23 +0530

    order chnanged

--- src/app/components/my-projects/personal_projectInfo.ts
+++ src/app/components/my-projects/personal_projectInfo.ts
@@ -15,42 +15,7 @@ export class Project {
 }
 
 export const PROJECTS = [
-  {
-    name: "Event Management",
-    description:
-      "A responsive event management web app built with React.js, designed to help users create, organize, and manage events seamlessly with a clean and modern user interface.",
-    tags: [
-      { name: "React", color: "blue-text-gradient" },
-      { name: "HTML & CSS", color: "green-text-gradient" },
-    ],
-    image: "assets/projectImg/event.png",
-    live: "https://eventmanagement-xi.vercel.app/",
-  },
-  {
-    name: "BTC Calculator",
-    description:
-      "A cryptocurrency calculator built with React that allows users to convert Bitcoin value into various global currencies using real-time exchange rates with a minimal and intuitive design.",
-    tags: [
-      { name: "React", color: "blue-text-gradient" },
-      { name: "REST API", color: "green-text-gradient" },
-      { name: "SCSS", color: "pink-text-gradient" },
-    ],
-    image: "assets/projectImg/btc.png",
-    live: "https://btc-calculator-rho.vercel.app/",
-  },
-  {
-    name: "Simple Calculator",
-    description:
-      "A responsive calculator built with Angular that performs basic arithmetic operations with a clean and modern UI. Designed for fast calculations and optimized for both desktop and mobile screens.",
-    tags: [
-      { name: "Angular", color: "blue-text-gradient" },
-      { name: "HTML & CSS", color: "green-text-gradient" },
-      { name: "TypeScript", color: "pink-text-gradient" },
-    ],
-    image: "assets/projectImg/calcy.png",
-    live: "https://calcysimple.vercel.app/",
-  },
-  {
+   {
     name: "Spot Drop",
     description:
       "A business promotion and deal management platform built with Angular and Firebase. It allows businesses to create profiles, post promotional deals, and manage them through an intuitive dashboard integrated with Google Maps and Firestore.",
@@ -95,4 +60,39 @@ export const PROJECTS = [
     image: "assets/projectImg/nodequest.png",
     live: "https://nodequest.vercel.app/",
   },
+  {
+    name: "Event Management",
+    description:
+      "A responsive event management web app built with React.js, designed to help users create, organize, and manage events seamlessly with a clean and modern user interface.",
+    tags: [
+      { name: "React", color: "blue-text-gradient" },
+      { name: "HTML & CSS", color: "green-text-gradient" },
+    ],
+    image: "assets/projectImg/event.png",
+    live: "https://eventmanagement-xi.vercel.app/",
+  },
+  {
+    name: "BTC Calculator",
+    description:
+      "A cryptocurrency calculator built with React that allows users to convert Bitcoin value into various global currencies using real-time exchange rates with a minimal and intuitive design.",
+    tags: [
+      { name: "React", color: "blue-text-gradient" },
+      { name: "REST API", color: "green-text-gradient" },
+      { name: "SCSS", color: "pink-text-gradient" },
+    ],
+    image: "assets/projectImg/btc.png",
+    live: "https://btc-calculator-rho.vercel.app/",
+  },
+  {
+    name: "Simple Calculator",
+    description:
+      "A responsive calculator built with Angular that performs basic arithmetic operations with a clean and modern UI. Designed for fast calculations and optimized for both desktop and mobile screens.",
+    tags: [
+      { name: "Angular", color: "blue-text-gradient" },
+      { name: "HTML & CSS", color: "green-text-gradient" },
+      { name: "TypeScript", color: "pink-text-gradient" },
+    ],
+    image: "assets/projectImg/calcy.png",
+    live: "https://calcysimple.vercel.app/",
+  } 
 ];
